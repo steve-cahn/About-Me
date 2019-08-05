5e68997ea080da36bbe6d9cd788d7a36cfe48439
Steve Cahn <steve@stevecahn.net>
2019-08-05 06:12:30 -0700

    Update portfolios.js

--- javascript/portfolios.js
+++ javascript/portfolios.js
@@ -42,7 +42,7 @@ function portfoliosData() {
 		{
 			title: 'phoneRX',
 			link: 'http://www.phonerx.stevecahn.net/',
-			imgSrc: 'https://i.postimg.cc/1zf5VmG1/phoneRX.gif',
+			imgSrc: 'https://media.giphy.com/media/U3De6Bp6JAJfWk0DYY/giphy.gif',
 			desc:
 				"<p>This is an eCommerce mockup website I built which sells phones</p><p>It took me a bit longer than a month to create it. I wasn't working on it full time due to my other responsibilities (such as my temporary job).</p><p>I used HTML5, sass, and JavaScript in vscode to create this website.</p><p>I definitely did learn a lot from creating this website, and got a lot more comfortable coding. The two biggest challenges, were:</p><ul><li>Figuring out a way to filter the products.</li><li>•	I totally forgot that when copying JavaScript objects, they copy as shallow copies, and not deep copies. When I tried updating the cart, there was this weird bug which I couldn't seem to figure out, until I remembered I had to create a deep copy on the object.</li></ul><p>All in all, it was very fun and a bit challenging creating such a website.</p>"
 		}
